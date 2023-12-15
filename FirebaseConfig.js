bc2af540334a5af88ff16cd29e8399c321d66d02
TshedisoB <tshedisoboshiana@gmail.com>
2023-12-15 12:11:40 +0200

    able to signin with no errors

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSn2SfbF0TUNxeb41N6eFbtgA81PpXr9E",
-  authDomain: "createuser-61694.firebaseapp.com",
-  projectId: "createuser-61694",
-  storageBucket: "createuser-61694.appspot.com",
-  messagingSenderId: "204082030380",
-  appId: "1:204082030380:web:02e403ea9c93568de6ae89",
-  measurementId: "G-RMGR4LR8G6",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
